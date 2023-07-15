4a00a7a1ea08e8e6060f13b92a1b44aee98fb8b1
Danila <icy.calmness@gmail.com>
2023-07-15 17:26:52 +0300

    create def main(); program application start

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.job_parser_application.job_parser import JobParser
+
+
+def main():
+    """
+    Главная функция, запускает приложение по поиску вакансий.
+    """
+
+    JobParser()
+
+
+if __name__ == "__main__":
+    main()
